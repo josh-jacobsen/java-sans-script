34a4fd936b617a3097784db69423a2d92675055d
josh-jacobsen <joshjacobsen5@gmail.com>
2016-08-18 08:17:38 +1200

    Initial project set up

---
+++ totalBeginner/src/org/totalBeginner/tutorial/Person.java
@@ -0,0 +1,30 @@
+package org.totalBeginner.tutorial;
+
+public class Person {
+	// fields
+	private String name; // the persons name 
+	private int maximumBooks; // the max number of books this person can borrow
+	
+	// constructors
+	public Person() {
+		name = "unknown name";
+		maximumBooks = 3;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String anyName) {
+		name = anyName;
+	}
+
+	public int getMaximumBooks() {
+		return maximumBooks;
+	}
+
+	public void setMaximumBooks(int maximumBooks) {
+		this.maximumBooks = maximumBooks;
+	}
+	
+
+}
